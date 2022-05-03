65c9c8608d5dcccd93f870d6d2c9d76276276098
Laís Gonçalves <80419962+lalinha123@users.noreply.github.com>
2022-05-03 20:36:34 -0300

    Update jogo_da_forca.py

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,22 +1,38 @@
+import os
+import time
+from sys import exit
+
 def jogar():
     palavra = str(input('Insira a palavra secreta: ')).upper()
     len_palavra = len(palavra)
     lst_texto = list(len_palavra * '*')
     lst_letra = []
-    max_tent = len_palavra + 6
+    max_tent = len_palavra + 4
     tentativas = 0
     pontos = 0
+    
+    def mostrar():
+        os.system('cls') or None
+        print(''.join(lst_texto))
+        
+    mostrar()
 
     while lst_texto != list(palavra) and tentativas < max_tent:
         letra = str(input('\nInsira uma letra: ')).upper()
         acertou = False
 
         while len(letra) > 1 or letra == '':
-            letra = str(input('Insira uma letra: ')).upper()
+            os.system('cls') or None
+            mostrar()
+            letra = str(input('\nInsira uma letra: ')).upper()
+            
+            tentativas += 1
         #-------FIM WHILE--------
 
         if letra in lst_letra:
-            print('Você já inseriu essa letra!')
+            lst_letra.append(letra)
+            mostrar()
+            print('Você já inseriu a letra',letra)
         else:
             x = 0
             while x < len_palavra:
@@ -24,6 +40,10 @@ def jogar():
                     lst_texto[x] = letra
                     acertou = True
                 x += 1
+            
+            lst_letra.append(letra)
+            mostrar()
+            
             #-------FIM WHILE--------
         #-------FIM IF ELSE--------
 
@@ -31,31 +51,32 @@ def jogar():
                 pontos += 1
         #-------FIM IF--------
 
-        lst_letra.append(letra)
-        print(''.join(lst_texto))
+        tentativas += 1
         
         if lst_texto == list(palavra):
+            os.system('cls') or None
             print('Parabéns! Você encontrou a palavra', palavra,'e fez',pontos,'ponto(s)!')
         #-------FIM IF--------
     #-------FIM WHILE--------
 
     if tentativas >= max_tent:
+        os.system('cls') or None
         print('Suas tentativas acabaram! Você fez',pontos,'ponto(s).')
         print('A palavra secreta era:', palavra)
     #-------FIM IF--------
-
+    
     resp = str(input('\nJogar novamente?: '))
     if resp == 's' or resp == 'sim':
+        os.system('cls') or None
         jogar()
     else:
+        os.system('cls') or None
         print('Obrigado por jogar! :D')
+        time.sleep(3)
+        os.system('cls') or None
+        exit(0)
+        
     #-------FIM IF ELSE--------
 #-------FIM DEF JOGAR()--------
 
 jogar()
-
-
-
-
-    
-
